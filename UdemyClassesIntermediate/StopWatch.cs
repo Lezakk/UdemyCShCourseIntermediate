60d0d7cdfd491c8dcfed905a9204f23b7c7b9f16
Lezakk <wiktro.zak94@gmail.com>
2020-03-17 20:50:51 +0100

    Add StopWatch class and StopWatchTests class, add first part of first exercise with test whitch check interval between start and stop, going to add methods and test to throw exception when start called twice and start timer again

---
+++ UdemyClassesIntermediate/StopWatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyClassesIntermediate
+{
+    public class StopWatch
+    {
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+        public void StartStopWatch()
+        {
+            Start = DateTime.Now;
+        }
+        public TimeSpan StopStopWatch()
+        {
+            Stop = DateTime.Now;
+            return Stop - Start;
+        }
+    }
+}
---
+++ UdemyClassesIntermediate_Tests/StopWatchTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using UdemyClassesIntermediate;
+using System.Threading;
+
+namespace UdemyClassesIntermediate_Tests
+{
+    [TestFixture]
+    class StopWatchTests
+    {
+        private StopWatch _stopWatch;
+        [SetUp]
+        public void SetUp()
+        {
+            _stopWatch = new StopWatch();
+        }
+        [TestCase(5000)]
+        [TestCase(12000)]
+        [TestCase(1000)]
+        public void Start_WhenStartMethodCalledStartTimerAndWhenStopMethodCalledStopTimer_ReturnsTimeFromStartToEnd(int time)
+        {
+            _stopWatch.StartStopWatch();
+            Thread.Sleep(time); // 5 sec 
+            var result =_stopWatch.StopStopWatch();
+            Assert.That(Math.Round(result.TotalSeconds,1),Is.EqualTo(TimeSpan.FromMilliseconds(time).TotalSeconds));
+        }
+
+        [Test]
+        public void Start_WhenStartMethodIsCalledTwiceInTheRow_ThrowException()
+        {
+            _stopWatch.StartStopWatch();
+            _stopWatch.StartStopWatch();
+            Assert.That(() => );
+        }
+
+    }
+}
