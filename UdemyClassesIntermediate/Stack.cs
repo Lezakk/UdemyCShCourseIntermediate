9c13a7031b153b36fce4121ed97b2e3ff036e61d
Lezakk <wiktro.zak94@gmail.com>
2020-03-24 18:36:44 +0100

    Developed test and add logic to exercise from section 4. Finished exxercise

--- UdemyClassesIntermediate/Stack.cs
+++ UdemyClassesIntermediate/Stack.cs
@@ -6,5 +6,25 @@ namespace UdemyClassesIntermediate
 {
     public class Stack
     {
+        public List<object> stackList = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj is null) throw new InvalidOperationException();
+            stackList.Add(obj);
+        }
+
+        public object Pop()
+        {
+            int lastItem = stackList.Count - 1;
+            if (stackList.Count == 0) throw new InvalidOperationException();
+            var result = stackList[lastItem];
+            stackList.RemoveAt(lastItem);
+            return result;
+        }
+        public void Clear()
+        {
+            stackList.Clear();
+        }
     }
 }
--- UdemyClassesIntermediate_Tests/StackTests.cs
+++ UdemyClassesIntermediate_Tests/StackTests.cs
@@ -28,18 +28,29 @@ namespace UdemyClassesIntermediate_Tests
         public void Push_WhenObjectIsPassed_ObjectIsOnTopOfStack(Object a)
         {
             _stack.Push(a);
-            Assert.That(_stack[_stack.Stack.Count - 1], Is.EqualTo(a));
+            Assert.That(_stack.stackList[_stack.stackList.Count - 1], Is.EqualTo(a));
         }
         [Test]
-        public void Pull_WhenStackIsEmptyAndPullIsCalled_ThrowsInvalidOperationException()
+        public void Pop_WhenStackIsEmptyAndPopIsCalled_ThrowsInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => _stack.Pull());
+            Assert.Throws<InvalidOperationException>(() => _stack.Pop());
+        }
+        [TestCase("Test",3)]
+        public void Pop_WhenPopIsCalled_ReturnsFirstObjectFromStackAndRemovesIt(Object firstObject, Object secondObject)
+        {
+            _stack.Push(firstObject);
+            _stack.Push(secondObject);
+            var stackQuantity = _stack.stackList.Count;
+            Assert.That(secondObject, Is.EqualTo(_stack.Pop()));
+            Assert.That(_stack.stackList.Count, Is.EqualTo(stackQuantity - 1));
+
         }
         [Test]
         public void Clear_WhenClearIsCalled_EmptyStackList()
         {
+            _stack.Push(765);
             _stack.Clear();
-            Assert.That(_stack.Stack.Count,Is.EqualTo(0));
+            Assert.That(_stack.stackList.Count,Is.EqualTo(0));
         }
 
     }
